987425464cd77361f93c6ac5a512f3dc60bc8781
zhe1ka <zheka.dzecina@gmail.com>
2023-04-25 09:26:27 +0300

    disable empty tests

--- backend/src/users/services/users.service.spec.ts
+++ backend/src/users/services/users.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { HttpModule } from '@nestjs/axios';
 
-fdescribe('UsersService', () => {
+xdescribe('UsersService', () => {
   let service: UsersService;
 
 
